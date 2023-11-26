d0f97c0d064bb1f4ee9603b1dfacf9863a117646
EmiriKO <71414805+EmiriKO@users.noreply.github.com>
2023-11-26 15:33:46 +0900

    refactor upload/download processes
    ・var -> const
    ・added comments
    ・added handling of cases where the uploaded file is not an .html
    ・added removeAllChildren func

--- src/script.js
+++ src/script.js
@@ -1,54 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // uploadボタンをクリックでアップロード処理をし、ダウンロード用リンクを作成する
-  uploadButton.addEventListener('change', function () {
-    var uploadFile = document.getElementById('uploadButton');
-    var file = uploadFile.files[0];
-    //const text = file.text()
-    if (!file) alert('ファイルを選択してください。');
+  // define constants
+  const formatErea = document.getElementById('formatErea');
+  const downloadErea = document.getElementById('downloadErea');
 
-    /********************************
-     * upload処理
-     ********************************/
-    // var uploadData;
-    // // var uploadText = document.getElementById('upload-text');
-    var reader = new FileReader();
+  // main process
+  uploadButton.addEventListener('change', function () {
+    const uploadFile = document.getElementById('uploadButton');
+    const file = uploadFile.files[0];    
     
+    // clear the format and download erea whenever upload button's input is changed
+    removeAllChildren(formatErea);
+    removeAllChildren(downloadErea);
+
+    // when no file is uploaded
+    if (!file){
+      alert('ファイルを選択してください。');
+      return;
+    }
+    // when uploaded file is not .html
+    if (file.type !== 'text/html') {
+      alert('HTMLファイルを選択してください。');
+      return;
+    }
+
+    // when uploaded file is html, read it
+    const reader = new FileReader();    
     reader.readAsText(file);
     reader.onload = function (e) {
       const originalLog = e.target.result;
-      const formattedLog = formatLog(originalLog);
+      const formattedLog = formatLog(originalLog);  // format uploaded log
 
-      var formatErea = document.getElementById('formatErea');
-      var downloadErea = document.getElementById('downloadErea');
-
-      const existingButton = formatErea.querySelector('button');
-      if (existingButton) {
-        formatErea.removeChild(existingButton);
-      }
-
-      var formatButton = document.createElement('button');
+      // create a format button 
+      const formatButton = document.createElement('button');
       formatButton.textContent = '整形！';
       formatErea.appendChild(formatButton);
 
+      // when format button is clicked
       formatButton.addEventListener('click', function () {
-        /********************************
-         * download用リンクの作成
-         ********************************/
         //reader.onloadが非同期処理なので、ちょっとだけ遅延させる
         setTimeout(function () {
-          // 既存のリンクを削除
-          const existingLink = downloadErea.querySelector('a');
-          if (existingLink) {
-            downloadErea.removeChild(existingLink);
-          }
-
-          //var downloadFile = JSON.stringify(text);
-          var downloadButton = document.createElement('a');
+          // clear the download erea and create a download button whenever format button is clicked
+          removeAllChildren(downloadErea);
+          const downloadButton = document.createElement('a');
           downloadButton.textContent = 'ダウンロード';
           downloadButton.download = 'formatted.html';
           downloadButton.href = 'data:text/html;charset=utf-8,' + encodeURIComponent(formattedLog);
-          //downloadButton.dataset.downloadurl = ['text/html', downloadButton.download, downloadButton.href].join(':');
-
           downloadErea.appendChild(downloadButton);
         }, 1000);
       })
@@ -56,8 +52,17 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 })
 
+// format log func
 function formatLog(originalLog){
   const formattedLog = originalLog;
   return formattedLog;
+}
+
+// remove all childern from a parent element
+function removeAllChildren(parent){
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+  return;
 }
 
